d308c3efabff6d43251039d81c426fe05b7bed1f
aa-nadim <nadim.ice.nstu@gmail.com>
2023-10-31 22:26:12 +0600

    test deploy_02

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # react-js-task_ollyo
 
 
+`npm run build`
+
+https://app.netlify.com/sites/react-js-task-ollyo/deploys
+
+Live site==>
+
+https://react-js-task-ollyo.netlify.app/
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          Nadim
+          Abdul Awal Nadim
         </h3>
         
       </header>
